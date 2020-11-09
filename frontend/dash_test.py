b89a49f535d029353b5b492c86babe122b7396f8
Yaroslav Vasyunin <y.vasyunin@icloud.com>
2020-11-09 17:25:11 +0100

    Connect to data from PostgreSQL

--- frontend/dash_test.py
+++ frontend/dash_test.py
@@ -1,13 +1,31 @@
 # -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+from os.path import expanduser
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+from sqlalchemy import create_engine
 import pandas as pd
+
+# DATA
+# Get connection parameters
+with open(expanduser('~/.pgpass'), 'r') as f:
+    for i, line in enumerate(f):
+        if i == 3:
+            host, port, db, user, password = line.split(':')
+
+# SQLAlchemy connectable
+# Scheme: "postgresql://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+db_uri = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}'
+con = create_engine(db_uri).connect()
+
+# Table named 'selfi4' will be returned as a dataframe.
+df = pd.read_sql_table('selfi4', con, schema='futurecraft')
+
+# WEB APP
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -15,13 +33,9 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Competenza": ["IoT", "3D printing", "Cloud Computing", "IoT", "3D printing", "Cloud Computing"],
-    "Quantità": [4, 1, 2, 2, 4, 5],
-    "Provincia": ["Vicenza", "Vicenza", "Vicenza", "Bolzano", "Treviso", "Salisburgo"]
-})
 
-fig = px.bar(df, x="Competenza", y="Quantità", color="Provincia", barmode="group")
+df_gr = df.groupby('settore')
+fig = px.bar(df, x='settore')
 
 app.layout = html.Div(children=[
     html.H1(children='Competenze FutureCRAFT'),

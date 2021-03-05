74ad5a71e25764f0141f13d90b891095578597a3
Yaroslav Vasyunin <yaroslav@digitalinnovationhubvicenza.it>
2021-03-05 17:29:32 +0100

    Change graphic and add real Digicheck Data

--- webapp/application.py
+++ webapp/application.py
@@ -14,11 +14,11 @@ import multilingual_content as ml
 
 # DATABASE CONNECTION
 HOST, PORT, DB, USER, PASSWORD = db_credentials()
-db_uri = f'postgresql+pg8000://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'  # pg8000==1.16.5 is important
+db_uri = f"postgresql+pg8000://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"  # pg8000==1.16.5 is important
 con = create_engine(db_uri, pool_pre_ping=True, pool_recycle=3600).connect()
 
 # Table named 'selfi4' will be returned as a dataframe.
-df = pd.read_sql_table('selfi4_group', con, schema='data')
+df = pd.read_sql_table("selfi4_group", con, schema="data")
 
 tech_top5 = ["Sistema gestionale", "Cloud", "Cybersicurezza e business continuity",
              "Sistemi di pagamento mobile e/o via Internet", "Sistemi di e-commerce e/o e-trade"]
@@ -32,21 +32,21 @@ app = dash.Dash(name=__name__,
 application = app.server
 
 fig = px.density_heatmap(df,
-                         labels={'tecnologia_fcraft': 'Competenza',
-                                 'provincia': 'Provincia',
-                                 'percento': 'Percentuale'},
-                         x=df['provincia'],
-                         y=df['tecnologia_fcraft'],
-                         z=df['percento'],
+                         labels={"tecnologia_fcraft": "Competenza",
+                                 "provincia": "Provincia",
+                                 "percento": "Percentuale"},
+                         x=df["provincia"],
+                         y=df["tecnologia_fcraft"],
+                         z=df["percento"],
                          color_continuous_scale=["#fff", "#fdcc0d"])
 
-fig.add_trace(go.Scatter(x=df['provincia'],
-                         y=df['tecnologia_fcraft'],
-                         mode='markers',
-                         marker=dict(size=np.where(df['formazione'], 10, 0),
-                                     color="black",
-                                     line=dict(width=2, color="black"),
-                                     symbol='x-thin'),
+fig.add_trace(go.Scatter(x=df["provincia"],
+                         y=df["tecnologia_fcraft"],
+                         mode="markers",
+                         marker=dict(size=df["formazione"]**2/10,  # size=np.where(df['formazione'], 10, 0
+                                     color="blue",
+                                     line=dict(width=2, color="blue"),
+                                     symbol="circle"),
                          hoverinfo='skip'))
 
 app.layout = html.Div([
@@ -75,14 +75,25 @@ app.layout = html.Div([
             dcc.Markdown(id="h2_ref")
         ]),
 
-        html.Div(className='eight columns div-for-charts',
-                 children=[dcc.Graph(id="fcraft-wp4",
-                                     figure=fig,
-                                     style={"margin-left": "50px"},
-                                     config={"displayModeBar": False, "scrollZoom": False}),
-            html.P("Top 5 delle tecnologie richieste:", style={"font-size": "120%", "margin-left": "50px"}),
-            html.Ol(id='tech-top5', children=[html.Li(tech) for tech in tech_top5], style={"margin-left": "50px"})
-        ])
+        html.Div(className="eight columns div-for-charts",
+                 children=[dcc.Tabs([
+                     dcc.Tab(label="BZ, TV, VI", children=[
+                         dcc.Graph(id="fcraft-wp4-ita",
+                                   figure=fig,
+                                   style={"margin-left": "50px"},
+                                   config={"displayModeBar": False, "scrollZoom": False})
+                     ]),
+                     dcc.Tab(label="SZG", children=[
+                         dcc.Graph(id="fcraft-wp4-aut",
+                                   figure=fig,
+                                   style={"margin-left": "50px"},
+                                   config={"displayModeBar": False, "scrollZoom": False})
+                     ])
+                 ]),
+                     html.P("Top 5 delle tecnologie richieste:", style={"font-size": "120%", "margin-left": "50px"}),
+                     html.Ol(id='tech-top5', children=[html.Li(tech) for tech in tech_top5],
+                             style={"margin-left": "50px"})
+                 ])
     ])
 ])
 
@@ -91,7 +102,7 @@ app.layout = html.Div([
     [Output("h1", "children"),
      Output("p_desc", "children"),
      Output("h2_ref", "children")],
-     Output("p_read_more", "children"),
+    Output("p_read_more", "children"),
     Output("p_dev_by", "children"),
     [Input("lang-selector", "value")])
 def multi_output(value):
